286da35d27ec17d41ff1ba4f91d4bea9825fa7cb
sigita <cepauskiki@gmail.com>
2018-09-17 18:46:09 +0300

    Heroku test

--- public/info.php
+++ public/info.php
@@ -1,26 +1,30 @@
 <?php
 
 echo "Start<br/>";
-$pass=getenv("DB_PASSWORD");
-$user=getenv("DB_USER");
-$host=getenv("DB_PASSWORD");
+$pass = getenv("DB_PASSWORD");
+$user = getenv("DB_USER");
+$host = getenv("DB_HOST");
 echo "Settings: $host $user $pass<br/>";
 
 
 use PDO;
+
 echo "Using PDO<br/>";
 
-$dbConnection = new PDO($host, $user, $pass);
-$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-echo "Connected<br/>";
-
-
 try {
-    $stmt = $this->dbConnection->prepare('select count(id) from orders');
-    $stmt->execute();
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    echo $stmt->fetchColumn(); //grazina array sudaryta visa is eiluciu
+    $dbConnection = new PDO($host, $user, $pass);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected<br/>";
+
+
+    try {
+        $stmt = $this->dbConnection->prepare('select count(id) from orders');
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        echo fetchColumnfetchColumn$stmt->fetchColumn(); //grazina array sudaryta visa is eiluciu
+} catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
