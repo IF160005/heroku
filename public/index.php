4b4a86aa2a2e3689215348baeac866f71f63143f
sigita <cepauskiki@gmail.com>
2018-09-17 23:48:26 +0300

    Change href values

--- public/index.php
+++ public/index.php
@@ -1,34 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors",1);
 
-function errorHandler($errno, $errstr, $errfile, $errline) {
-
-    switch ($errno) {
-        case E_NOTICE:
-        case E_USER_NOTICE:
-        case E_DEPRECATED:
-        case E_USER_DEPRECATED:
-        case E_STRICT:
-            echo("STRICT error $errstr at $errfile:$errline n");
-            break;
-
-        case E_WARNING:
-        case E_USER_WARNING:
-            echo("WARNING error $errstr at $errfile:$errline n");
-            break;
-
-        case E_ERROR:
-        case E_USER_ERROR:
-        case E_RECOVERABLE_ERROR:
-            exit("FATAL error $errstr at $errfile:$errline n");
-
-        default:
-            exit("Unknown error at $errfile:$errline n");
-    }
-}
-
-set_error_handler("errorHandler");
 require __DIR__ . "/../vendor/autoload.php";
 
 session_start();
