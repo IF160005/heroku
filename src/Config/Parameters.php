78ac43bacafef48fd4c05250ae4bd2e221ed9ce1
sigita <cepauskiki@gmail.com>
2018-09-17 15:08:18 +0300

    changes use get env instead $_env

--- src/Config/Parameters.php
+++ src/Config/Parameters.php
@@ -14,26 +14,27 @@ class Parameters
 
     public static function getConnectionUrl()
     {
-        return isset($_ENV["DB_HOST"]) ? $_ENV["DB_HOST"] :  self::$connectionUrl;
+
+        return getenv("DB_HOST") ?  getenv("DB_HOST") :  self::$connectionUrl;
     }
 
     public static function getConnectionUser()
     {
-        return isset($_ENV["DB_USER"]) ? $_ENV["DB_USER"] : self::$connectionUser;
+        return getenv("DB_USER") ?  getenv("DB_USER") : self::$connectionUser;
     }
 
     public static function getConnectionPassword()
     {
-        return isset($_ENV["DB_PASSWORD"]) ? $_ENV["DB_PASSWORD"] :  self::$connectionPassword;
+        return  getenv("DB_PASSWORD") ? getenv("DB_PASSWORD") :  self::$connectionPassword;
     }
 
     public static function getLogin()
     {
-        return isset($_ENV["ADMIN_LOGIN"]) ? $_ENV["ADMIN_LOGIN"] :  self::$login;
+        return  getenv("ADMIN_LOGIN") ? getenv("ADMIN_LOGIN") :  self::$login;
     }
 
     public static function getPassword()
     {
-        return isset($_ENV["ADMIN_PASSWORD"]) ? $_ENV["ADMIN_PASSWORD"] :  self::$password;
+        return getenv("ADMIN_PASSWORD") ? getenv("ADMIN_PASSWORD") :  self::$password;
     }
 }
